f2fc2d12bd46fad3359db2291454aeeda706e894
cat-mem <115585790+cat-mem@users.noreply.github.com>
2023-01-16 18:30:10 +0300

    fix main js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
 $('.header').clone(true).unwrap().appendTo('.hide-header');
+$('.goToPrice').click(function() { $('html, body').animate({scrollTop: $('.reviews .separation').offset().top + 'px'}) })
 
-
-$('.goToPrice').click(function() { 
-    $('html, body').animate({scrollTop: $('.reviews .separation').offset().top + 'px'})
-})
-
-
-
-
-var isScroll = 0, // доп. проверка
-    targetScroll = 70; // расстояние до действия / в px
-    
+var isScroll = 0, targetScroll = 70;
 $(window).on('scroll', function(){
   if(isScroll === 0 && $(this).scrollTop() >= targetScroll) {
     isScroll = 1;
     $(".hide-header").css({"top":"0", "opacity":"1"});
-    console.log('12');
   } else if(isScroll === 1 && $(this).scrollTop() < targetScroll) {
     isScroll = 0;
     $(".hide-header").css({"top":"-75px", "opacity":"0"});
   }
 });
 
-console.log($('link').attr("rel") == "icon")
 const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
 if (darkThemeMq.matches) {
     $('link[rel~="icon"]').attr('href', 'img/FaviconLight.png');
